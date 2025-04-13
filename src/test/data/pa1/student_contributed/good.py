a2448cf04147c5cd2cc9e03663c75ea9177dd8fe
MrDemiurgos <davidvazquez@id.uff.br>
2025-04-13 02:04:51 -0300

    good.py feito

--- src/test/data/pa1/student_contributed/good.py
+++ src/test/data/pa1/student_contributed/good.py
@@ -1,1 +1,26 @@
-1 + 2 + 3
+David: bool = True
+Erik: bool = True
+Paulo: bool = True
+
+Cristiano: object = None
+
+professor: int = 2
+
+class CristianoBraga(object):
+    orientador: object = None
+
+class Vazquez(object):
+    valor: int = 2
+
+grupo: CristianoBraga = None
+alunos: [Vazquez] = None
+
+mensagem: str = "hi"
+objeto: object = None
+
+print(1 + professor + 3)
+
+
+
+
+
