57b2fa240830965d372f1cd2dbec6f67bc2e428a
MrDemiurgos <davidvazquez@id.uff.br>
2025-04-13 13:07:24 -0300

    good.py atualizada

--- src/test/data/pa1/student_contributed/good.py
+++ src/test/data/pa1/student_contributed/good.py
@@ -1,26 +1,40 @@
-David: bool = True
-Erik: bool = True
-Paulo: bool = True
+class RodaLoop(object):
 
-Cristiano: object = None
+    David: bool = True
+    Erik: bool = False
+    Paulo: bool = True
+    variavel1: int = 0
 
-professor: int = 2
+    def __init__(self:"RodaLoop", roda: bool, count: int):
+        self.rodando = roda
+        self.contador = count
+        
+    def loop(self:"RodaLoop") -> int:
 
-class CristianoBraga(object):
-    orientador: object = None
+        while self.rodando:
+            self.contador = self.contador + 1
+            if self.contador > 10:
+                self.rodando = False
+        return self.contador
+    
+    
+def testaDeclaracao(x: int, lista: [int]) -> int:
+    global variavel1
+    variavel1 = 2 + 2
 
-class Vazquez(object):
-    valor: int = 2
+    for i in range(0, len(lista)):
+        if lista[i] == 1:
+            x = x + 1
+        else:
+            x = x - 1
+    return x
+    
 
-grupo: CristianoBraga = None
-alunos: [Vazquez] = None
+print(RodaLoop(True, 0).loop())
+print(testaDeclaracao(1, [1, 2, 3]))
 
-mensagem: str = "hi"
-objeto: object = None
+mensagem = "teste"
+professor = "christiano"
 
+# teste comentario
 print(1 + professor + 3)
-
-
-
-
-
